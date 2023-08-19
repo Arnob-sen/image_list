d720f9a2d0a7809b2434ad9d4e70dcaa1602196d
Arnob-sen <arnobsen16.2@gmail.com>
2023-08-19 14:27:18 +0600

    handle input change

--- src/component/SearchInput.js
+++ src/component/SearchInput.js
@@ -1,5 +1,9 @@
 import React from "react";
 class SearchInput extends React.Component{
+    onChangeinput(event)
+    {
+        console.log(event.target.value)
+    }
     render()
     {
         return(
@@ -8,7 +12,9 @@ class SearchInput extends React.Component{
                 <form className="ui form">
                     <div className="field">
                         <div className="ui massive icon input">
-                      <input type="text" placeholder="search..."/>
+                      <input type="text" placeholder="search..."
+                      onChange={this.onChangeinput}// on submit or on click,must use onChange or onSubmit or onClick.otherwise function will not call
+                      />
                       <i className="search icon"></i>
                       </div>
                       </div>
