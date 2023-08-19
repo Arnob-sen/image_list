de7f6478e02f8b3bae81ba2afd69a7d6a3d31d46
Arnob-sen <arnobsen16.2@gmail.com>
2023-08-19 13:51:45 +0600

    create_searchinput-component

---
+++ src/component/SearchInput.js
@@ -0,0 +1,17 @@
+import React from "react";
+class SearchInput extends React.Component{
+    render()
+    {
+        return(
+            <div className="search-input">
+                
+                <form>
+                      <input type="text"/>
+                    
+                    </form>
+                
+                </div>
+        )
+    }
+}
+export default SearchInput
