efe83ee5c00156a55812e2e29c8278d60339f4b2
Arnob-sen <arnobsen16.2@gmail.com>
2023-08-21 00:35:15 +0600

    create_image_list_according_to_the search

--- src/component/app.js
+++ src/component/app.js
@@ -1,6 +1,7 @@
 import React from "react";
 import SearchInput from './SearchInput';
 import axios from "axios";
+import ImageList from "./Image_list";
 class App extends React.Component
 {
     constructor(props)
@@ -20,7 +21,7 @@ class App extends React.Component
     return(
         <div className="ui container" style={{marginTop:'30px'}}>
             <SearchInput onSearchSubmit={this.onSearchSubmit} />
-            we have {this.state.images.length} images
+         <ImageList images={this.state.images}/>
         </div>
     )
     }
