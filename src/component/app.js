b007138b377c02ee1fda05ee3eb861498b557759
Arnob-sen <arnobsen16.2@gmail.com>
2023-08-20 23:14:48 +0600

    make an arrayh of images

--- src/component/app.js
+++ src/component/app.js
@@ -6,18 +6,21 @@ class App extends React.Component
     constructor(props)
     {
         super(props);
+        this.state={images:[]}
         this.onSearchSubmit=this.onSearchSubmit.bind(this)
     }
    async onSearchSubmit(entry){//creating call back function
         
       const response= await axios.get(`https://pixabay.com/api/?key=38951609-f57c19e63bbf7fc1eb95c3495&q=${entry}&image_type=photo`)
       console.log(response.data.hits);
+      this.setState({images:response.data.hits});
     }
         
     render(){
     return(
         <div className="ui container" style={{marginTop:'30px'}}>
             <SearchInput onSearchSubmit={this.onSearchSubmit} />
+            we have {this.state.images.length} images
         </div>
     )
     }
